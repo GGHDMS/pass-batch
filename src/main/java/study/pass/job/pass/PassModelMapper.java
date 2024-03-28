d64b8e0b73bda882d8482aa854dc0314eb911481
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-03-28 16:40:29 +0900

    Batch 이용권 일괄 지급

---
+++ src/main/java/study/pass/job/pass/PassModelMapper.java
@@ -0,0 +1,29 @@
+package study.pass.job.pass;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+import study.pass.repository.pass.BulkPassEntity;
+import study.pass.repository.pass.BulkPassStatus;
+import study.pass.repository.pass.PassEntity;
+import study.pass.repository.pass.PassStatus;
+
+// ReportingPolicy.IGNORE: 일치하지 않은 필드를 무시합니다.
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PassModelMapper {
+    PassModelMapper INSTANCE = Mappers.getMapper(PassModelMapper.class);
+
+    // 필드명이 같지 않거나 custom하게 매핑해주기 위해서는 @Mapping을 추가해주면 됩니다.
+    @Mapping(target = "status", qualifiedByName = "status")
+    @Mapping(target = "remainingCount", source = "bulkPassEntity.count")
+    PassEntity toPassEntity(BulkPassEntity bulkPassEntity, String userId);
+
+    // BulkPassStatus와 관계 없이 PassStatus값을 설정합니다.
+    @Named("status")
+    default PassStatus status(BulkPassStatus status) {
+        return PassStatus.READY;
+    }
+
+}
---
+++ src/main/java/study/pass/repository/pass/BulkPassStatus.java
@@ -0,0 +1,5 @@
+package study.pass.repository.pass;
+
+public enum BulkPassStatus {
+    READY, COMPLETED
+}
